fd65fc7a32c369800a249eaa7c2a4466b1bb8f12
Jack DeVries <jdevries3133@gmail.com>
2026-02-20 19:13:21 -0500

    feat: cache token in environment

--- src/main.zig
+++ src/main.zig
@@ -2,20 +2,19 @@ const std = @import("std");
 const libj = @import("root.zig");
 const dbg = libj.dbg;
 
-
 pub fn main(init: std.process.Init.Minimal) !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-
     const alloc = arena.allocator();
-
-    var threaded_io = std.Io.Threaded.init(alloc, .{
-        .environ = init.environ
-    });
+    var threaded_io = std.Io.Threaded.init(alloc, .{ .environ = init.environ });
     defer threaded_io.deinit();
-
     const io = threaded_io.io();
-    const response = try libj.google_oauth.authenticate(alloc, io, init.environ);
-    defer response.deinit();
 
-    dbg(@src(), "access token: {s}\n", . { response.value.access_token });
+    const oauth_access_token = std.process.Environ.getPosix(init.environ, "GOOGLE_OAUTH_ACCESS_KEY") orelse blk: {
+        const response = try libj.google_oauth.authenticate(alloc, io, init.environ);
+        defer response.deinit();
+        const access_token = try alloc.dupe(u8, response.value.access_token);
+        break :blk access_token;
+    };
+
+    dbg(@src(), "token: {s}\n", .{oauth_access_token});
 }
